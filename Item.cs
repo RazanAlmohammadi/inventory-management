061cb876b1f96df8e2d9bc471783325ad0d1f18c
razan4577 <razanalmohammadi@icloud>
2024-09-03 12:24:00 +0300

    level 1

--- Item.cs
+++ Item.cs
@@ -13,12 +13,36 @@ using System.Threading.Tasks;
 
     public Item(string name, int quantity, DateTime? createdDate = null)
     {
-        
         _name = name;
+        if (quantity < 0)
+        {
+            Console.WriteLine("Quantity cannot be negative");
+        }
         _quantity = quantity;
         _createdDate = createdDate ?? DateTime.Now;
-
-        
     }
-    
+
+    public string Name
+    {
+        get { return _name; }
+    }
+
+    public int Quantity
+    {
+        get { return _quantity; }
+        set
+        {
+            if (value < 0)
+            {
+                Console.WriteLine("Quantity cannot be negative");
+            }
+            _quantity = value;
+        }
+    }
+
+    public DateTime CreatedDate
+    {
+        get { return _createdDate; }
+    }
+
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+var waterBottle = new Item("Water Bottle", 10, new DateTime(2023, 1, 1));
+var chocolateBar = new Item("Chocolate Bar", 15, new DateTime(2023, 2, 1));
+Console.WriteLine($"Item Name: {waterBottle.Name}, Quantity: {waterBottle.Quantity}, Created Date: {waterBottle.CreatedDate}");
+Console.WriteLine($"Item Name: {chocolateBar.Name}, Quantity: {chocolateBar.Quantity}, Created Date: {chocolateBar.CreatedDate}");
+var coffee = new Item("Coffee", 20);
+Console.WriteLine($"Item Name: {chocolateBar.Name}, Quantity: {chocolateBar.Quantity}");
